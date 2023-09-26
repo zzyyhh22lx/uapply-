41412ba099186b97a8ff97a0788cf0da36da21dc
zzyyhh22lx <2744732798@qq.com>
2023-09-26 21:08:42 +0800

    [feat: 搭好Vue项目框架]

---
+++ frontend/src/views/components/MarkdownEditor.vue
@@ -0,0 +1,7 @@
+<template>
+    <div v-html="transformStr('<div>测试</div>')"></div>
+</template>
+<script setup lan="ts">
+import { transformStr } from '@/libs/utils.ts';
+
+</script>
---
+++ frontend/src/views/home.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <markdown-editor />
+    </div>
+</template>
+<script setup lan="ts">
+import MarkdownEditor from './components/MarkdownEditor.vue';
+
+</script>
